8bc530e3ba81a5e27fa4f13ab22009a78a1c18c5
Anibal Linares <48930195+anibal85@users.noreply.github.com>
2020-01-15 14:30:04 -0300

    Create Query_hive.sql

---
+++ Query_hive.sql
@@ -0,0 +1,29 @@
+Create and Load data in Hive table
+
+CREATE DATABASE IF NOT EXISTS employee LOCATION '/data/employee';
+
+hdfs -dsf -put employee /user/employee
+
+hdfs -dsf -cat/user/employee
+
+create table employee(
+id int,
+name string,
+city string,
+departament string,
+salary int) 
+row formate delimited
+fields terminated by '~';
+
+show table;
+
+LOAD DATA LOCATL INPATH '/user/employee' OVERWRITE INTO TABLE employee;
+
+SELECT * FROM employee;
+
+---------------------------------------------------------------------------------------
+
+ Hive Aggregation Functions
+
+
+
